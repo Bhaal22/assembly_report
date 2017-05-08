cadbd5dea305081e803fc41319d9e918c834213e
Bhaal22 <muller.john@gmail.com>
2017-05-08 17:52:43 +0200

    initial commit

---
+++ assembly_report/AssemblyReport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace report
+{
+    class AssemblyReport
+    {
+        static void Main(string[] args)
+        {
+            var filename = args[0];
+
+            try
+            {
+                var loadedAssembly = Assembly.LoadFile($@"{filename}");
+                var references = loadedAssembly.GetReferencedAssemblies();
+
+                Console.WriteLine($"Success {filename}");
+                foreach (var reference in references)
+                {
+                    Console.WriteLine($"{reference.Name} {reference.Version}");
+                }
+                Console.WriteLine($"Done {filename}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Fail {filename}: {ex.Message}");
+            }
+        }
+    }
+}
